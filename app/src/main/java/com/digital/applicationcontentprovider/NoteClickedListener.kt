fd3b011ce27cdbbd5ad3030df6bc4b0ab23575d7
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-22 08:06:14 -0300

    Criando a interface NoteClickedListener

---
+++ app/src/main/java/com/digital/applicationcontentprovider/NoteClickedListener.kt
@@ -0,0 +1,8 @@
+package com.digital.applicationcontentprovider
+
+import android.database.Cursor
+
+interface NoteClickedListener {
+    fun noteClickedItem(cursor: Cursor)
+    fun noteRemoveItem(cursor: Cursor?)
+}
--- app/src/main/java/com/digital/applicationcontentprovider/NotesAdapter.kt
+++ app/src/main/java/com/digital/applicationcontentprovider/NotesAdapter.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.digital.applicationcontentprovider.database.NotesDatabaseHelper.Companion.DESCRIPTION_NOTES
+import com.digital.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 
-class NotesAdapter(): RecyclerView.Adapter<NotesViewHolder>() {
+class NotesAdapter(
+    private val listener: NoteClickedListener
+): RecyclerView.Adapter<NotesViewHolder>() {
 
     private var mCursor: Cursor? = null
 
@@ -16,12 +20,22 @@ class NotesAdapter(): RecyclerView.Adapter<NotesViewHolder>() {
         NotesViewHolder(LayoutInflater.from(parent.context)
             .inflate(R.layout.note_item, parent, false))
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = if (mCursor != null) mCursor?.count as Int else 0
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        mCursor?.moveToPosition(position)
+
+        holder.noteTitle.text = mCursor?.getString(mCursor?.getColumnIndex(TITLE_NOTES) as Int)
+        holder.noteDescription.text = mCursor?.getString(mCursor?.getColumnIndex(DESCRIPTION_NOTES) as Int)
+        holder.noteButtonRemove.setOnClickListener {
+            mCursor?.moveToPosition(position)
+            listener.noteRemoveItem(mCursor)
+            notifyDataSetChanged()
+        }
+
+        holder.itemView.setOnClickListener {
+            listener.noteClickedItem(mCursor as Cursor)
+        }
     }
 
     fun setCursor(newCursor: Cursor) {
