99f2925d04dceb39e07811139054204b2eba4cc2
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-22 07:36:43 -0300

    complementing CardView with ConstraintLayout

--- app/src/main/java/com/digital/applicationcontentprovider/MainActivity.kt
+++ app/src/main/java/com/digital/applicationcontentprovider/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.digital.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 import com.digital.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
     lateinit var noteRecyclerView: RecyclerView
     lateinit var noteAdd: FloatingActionButton
 
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +25,12 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
         noteAdd = findViewById(R.id.note_add)
         noteAdd.setOnClickListener {  }
 
+        adapter = NotesAdapter()
+        adapter.setHasStableIds(true)
+
         noteRecyclerView = findViewById(R.id.notes_recycler)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
